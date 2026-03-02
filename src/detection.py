239f8ae4861f9111d387c0ffb6e75eff08159903
abegole <abegole@g.ucla.edu>
2026-03-02 18:35:13 +0800

    Fixed with Python 3.11 venv, MTCNN import works now. Make sure to install correct python ver and make a virtual environment (venv)

--- src/detection.py
+++ src/detection.py
@@ -1,2 +1,11 @@
 from mtcnn import MTCNN
 
+from mtcnn.utils.images import load_image
+from mtcnn.utils.plotting import plot
+import matplotlib.pyplot as plt
+
+image = load_image("../data/Aiden1.jpg")
+
+mtcnn = MTCNN(device="CPU:0")   
+
+result = mtcnn.detect_faces(image)
